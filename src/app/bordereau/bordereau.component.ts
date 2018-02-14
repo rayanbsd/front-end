0f5e4ca0a5bc7c2e2dcf50892aed8dcd1701fbe6
kais_fayech <kais_fayech@bitbucket.org>
2018-02-14 11:43:41 +0100

    first commit

---
+++ src/app/bordereau/bordereau.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bordereau',
+  templateUrl: './bordereau.component.html',
+  styleUrls: ['./bordereau.component.css']
+})
+export class BordereauComponent implements OnInit {
+  val1: number=0.0;
+  val2:number=0.0;
+  val3:number=0.0; 
+
+  val4:number=0.0; 
+  val5:number=0.0; 
+  val6:number=0.0; 
+  val7:number=0.0; 
+  val8:number=0.0;   
+  val9:number=0.0;
+ 
+  val10:number=0.0;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
